009690d69405c836c13dd95b61c75de639bec715
JackSerafini <lagiacromo2803@gmail.com>
2024-04-25 21:58:58 +0200

    Added more comments

--- WebCrawler.py
+++ WebCrawler.py
@@ -92,6 +92,9 @@ class WebCrawler():
         """
         Handle each task/worker by running a loop that stops only when the task is cancelled.
 
+        When using workers, multiple parallel coroutines or tasks are created and they run in the same event loop but are dedicated to performing a specific subset of tasks 
+        repeatedly. In the context of a web crawler, each worker continuously fetch URLs from a queue, process them, and possibly enqueue more URLs.
+
         Parameters:
             session: The session created when all the workers are initialised used to handle client requests.
         """
@@ -113,6 +116,8 @@ class WebCrawler():
         Parameters:
             session: The session created when all the workers are initialised used to handle client requests.
         """
+        # If you want to add a delay in between requests (it does impact greatly on performances)
+        # asyncio.sleep(.1)
         # First get the first URL of the queue
         url = await self.queue.get()
 
@@ -179,9 +184,9 @@ class WebCrawler():
                     await self.queue.put(absolute_url)
 
 if __name__ == "__main__":
-    crawler = WebCrawler("https://books.toscrape.com/")
+    crawler = WebCrawler("https://books.toscrape.com/") # Change here the root URL (tried examples: https://books.toscrape.com/ , https://quotes.toscrape.com/ , https://www.subito.it/)
     start = time.perf_counter()
-    asyncio.run(crawler.crawl(), debug=True)
+    asyncio.run(crawler.crawl())
     end = time.perf_counter()
     seen = sorted(crawler.visited)
     print("Results:")
@@ -194,26 +199,12 @@ if __name__ == "__main__":
 # asynchronous crawler DFS done in around 5 seconds
 # asynchronous crawler BFS done in around 2 seconds with 5 workers
 
-# When using workers, you're essentially creating multiple parallel coroutines or tasks 
-# that run in the same event loop but are dedicated to performing a specific subset of tasks 
-# repeatedly. In the context of a web crawler, each worker might continuously fetch URLs from a 
-# queue, process them, and possibly enqueue more URLs.
-
-# Fairness
-# A "fair" crawler operates in a way that respects the websites it visits. This involves several specific practices:
-
-# Rate Limiting: A fair crawler does not overwhelm a website's server by making too many requests in a short period. It respects the site's robots.txt rules regarding crawl rate and observes polite intervals between requests to prevent undue load on the servers.
-# Obeying robots.txt: This file on websites specifies which parts of the site should not be crawled. A fair crawler respects these rules and avoids crawling disallowed areas, thereby adhering to the website's wishes and legal guidelines.
-# User-Agent Transparency: It clearly identifies itself with a truthful and informative User-Agent string. This allows website administrators to distinguish the crawler from normal user traffic, trace its activity, and configure server-side rules specifically for it if necessary.
-# Bandwidth Consideration: The crawler minimizes the bandwidth it consumes from each website, understanding that excessive use of bandwidth can interfere with the normal function of the website, affecting its ability to serve content to human users.
-
-# Robustness
-# A "robust" crawler is designed to handle various operational challenges effectively:
-
-# Error Handling: It can gracefully handle errors encountered during the crawling process, such as HTTP errors, connection timeouts, or redirects. A robust crawler is prepared to retry failed requests, but with an appropriate delay and a limit on retries to avoid causing issues for the website.
-# Adaptability: It can manage different website structures and content types, adapting its crawling strategy as needed. This includes handling dynamic content delivered via JavaScript, parsing multiple content types, and following embedded links in various formats.
-# Scalability: It can efficiently manage resources to handle large-scale operations. This includes scaling up to crawl more sites or more extensive areas of a site without a significant drop in performance or reliability.
-# Resilience to Anti-Crawling Techniques: Websites may employ techniques to block or restrict automated crawlers. A robust crawler might use strategies to ethically navigate these controls, always in compliance with legal boundaries and ethical standards.
-# Content Parsing and Extraction: The ability to accurately parse and extract useful data from diverse and complex web pages is crucial for a robust crawler. This involves dealing with different encodings, languages, and rapidly changing web standards.
-
-# Check this info and maybe add like a paragraph explaining the choices made
+"""
+The crawler has to be both fair and robust:
+- Fairness: a "fair" crawler operates in a way that respects the websites it visits.
+For this reason, the crawler obeys to the robots.txt file to check which parts of the site should not be crawled. In addition, a small delay between requests can be added to limit the rate at which the crawler sends 
+requests to the website, to not overwhelm the server.
+- Robustness: a "robust" crawler is designed to handle various operational challenges.
+The crawler catches errors entountered during the crawling process (HTTP errors, connection timeouts, etc..) and prints what the problem was. Thanks to the use of workers, this crawler can also efficiently handle large-scale
+operations. Moreover, since the search implemented is a BFS, the crawler does not get stuck in loops and does not fall for anti-crawling techniques.
+"""
